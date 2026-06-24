c439511f0315584d9737a7df15e54e7339ae9ea5
gowri018 <gowria668@gmail.com>
2026-06-24 11:38:30 +0530

    design pattern

---
+++ Week 1/Design patterns/Exercise2_FactoryMethod/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        DocumentFactory factory;
+
+        factory = new WordFactory();
+
+        Document document = factory.createDocument();
+
+        document.open();
+    }
+}
