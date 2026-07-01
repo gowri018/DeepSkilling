a14d44f875aa9aef2f8251b6c651de7d66fb2c9c
gowri018 <gowria668@gmail.com>
2026-07-01 22:46:16 +0530

    Implementing Constructor and Setter Injection

---
+++ Week 2/Spring Core_Maven/LibraryManagement/src/main/java/com/library/aspect/LoggingAspect.java
@@ -0,0 +1,24 @@
+package com.library.aspect;
+
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+
+@Aspect
+public class LoggingAspect {
+
+    @Around("execution(* com.library.service.*.*(..))")
+    public Object logExecutionTime(ProceedingJoinPoint joinPoint) throws Throwable {
+
+        long start = System.currentTimeMillis();
+
+        Object result = joinPoint.proceed();
+
+        long end = System.currentTimeMillis();
+
+        System.out.println("Method : " + joinPoint.getSignature().getName());
+        System.out.println("Execution Time : " + (end - start) + " ms");
+
+        return result;
+    }
+}
